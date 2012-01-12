e0fc7185e12135142b838d6da61996be27fdfb9e
Yang Chen <chenyang@cs.utah.edu>
2012-01-12 21:39:24 +0000

    Fire a rewriter on the designated parameter of a given function declaration candidate:
      - replace it with void if this parameter is the only one
    
      - otherwise remove it

--- FuncParamReplacement.cpp
+++ FuncParamReplacement.cpp
@@ -6,6 +6,7 @@
 #include "clang/Parse/ParseAST.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Rewrite/Rewriter.h"
+#include "clang/Basic/SourceManager.h"
 
 #include "TransformationManager.h"
 
@@ -31,14 +32,15 @@ public:
 private:
   FPRASTConsumer *ConsumerInstance;
 
-  void rewriteFuncDecl(FunctionDecl *FP);
+  bool rewriteFuncDecl(FunctionDecl *FP);
 
+  bool rewriteParam(const ParmVarDecl *PV, 
+                    unsigned int NumParams);
+
+  void insertAndRemove(Rewriter &RW, 
+                       SourceRange OrigRange,
+                       const char *ReplacementStr);
 };
-
-void FPRASTVisitor::rewriteFuncDecl(FunctionDecl *FP) 
-{
-  // TODO
-}
 
 class FPRASTConsumer : public ASTConsumer {
   friend class FPRASTVisitor;
@@ -51,17 +53,19 @@ public:
 
   virtual void Initialize(ASTContext &context) {
     Context = &context;
+    SrcManager = &Context->getSourceManager();
     TransformationASTVisitor = new FPRASTVisitor(this);
     TheRewriter.setSourceMgr(Context->getSourceManager(), 
                              Context->getLangOptions());
+
     ValidInstanceNum = 0;
     TransError = TransSuccess;
     TheFuncDecl = NULL;
     TheParamPos = -1;
   }
 
-  bool notMaxInstanceError(void) {
-    return (TransError != TransMaxInstanceError);
+  bool transSuccess(void) {
+    return (TransError == TransSuccess);
   }
   
   bool transInternalError(void) {
@@ -79,6 +83,10 @@ public:
 
   virtual void HandleTranslationUnit(ASTContext &Ctx);
 
+  void outputTransformedSource(void);
+
+  void outputOriginalSource(void);
+
   ~FPRASTConsumer(void) {
     delete TransformationASTVisitor;
   }
@@ -88,6 +96,8 @@ private:
   SmallVector<FunctionDecl *, 10> ValidFuncDecls;
 
   ASTContext *Context;
+
+  SourceManager *SrcManager;
 
   FPRASTVisitor *TransformationASTVisitor;
 
@@ -105,6 +115,82 @@ private:
 
   bool isValidFuncDecl(FunctionDecl *FD);
 };
+ 
+bool FPRASTVisitor::rewriteParam(const ParmVarDecl *PV, 
+                                 unsigned int NumParams)
+{
+  SourceRange ParamLocRange = PV->getSourceRange();
+  SourceLocation StartLoc = ParamLocRange.getBegin();
+  int RangeSize = 
+    ConsumerInstance->TheRewriter.getRangeSize(ParamLocRange);
+
+  if (RangeSize == -1)
+    return false;
+
+  // The param is the only parameter of the function declaration.
+  // Replace it with void
+  if ((ConsumerInstance->TheParamPos == 0) && (NumParams == 1)) {
+    // Note that ')' is included in ParamLocRange
+    // insertAndRemove(ConsumerInstance->TheRewriter, ParamLocRange, "void");
+    return ConsumerInstance->TheRewriter.ReplaceText(StartLoc, 
+                                                     RangeSize - 1, "void");
+  }
+
+  // The param is the last parameter
+  if (ConsumerInstance->TheParamPos == static_cast<int>(NumParams - 1)) {
+    int offset = 0;
+    const char *StartBuf = 
+      ConsumerInstance->SrcManager->getCharacterData(StartLoc);
+
+    assert(StartBuf && "Invalid start buffer!");
+    while (*StartBuf != ',') {
+      StartBuf--;
+      offset--;
+    }
+
+    SourceLocation NewStartLoc = 
+      StartLoc.getLocWithOffset(offset);
+    return ConsumerInstance->TheRewriter.RemoveText(NewStartLoc, 
+                                                    RangeSize - offset - 1);
+  }
+ 
+  // The param is in the middle
+  return ConsumerInstance->TheRewriter.RemoveText(StartLoc, RangeSize);
+}
+
+bool FPRASTVisitor::rewriteFuncDecl(FunctionDecl *FD) 
+{
+  const ParmVarDecl *PV = NULL;
+  int ParamPos = -1;
+
+  // Get the parameter to replace
+  for (FunctionDecl::param_const_iterator PI = FD->param_begin(),
+       PE = FD->param_end(); PI != PE; ++PI) {
+    ParamPos++;
+    if (ParamPos == ConsumerInstance->TheParamPos) {
+      PV = (*PI);
+      break;
+    }
+  }
+
+  assert(PV && "Unmatched ParamPos!");
+  if (!rewriteParam(PV, FD->getNumParams()))
+    return false;
+
+  if (FD->isThisDeclarationAsDefinition()) {
+
+  }
+  return true;
+}
+
+bool FPRASTVisitor::VisitFunctionDecl(FunctionDecl *FD) {
+  FunctionDecl *CanonicalFD = FD->getCanonicalDecl();
+
+  if (CanonicalFD == ConsumerInstance->TheFuncDecl)
+    return rewriteFuncDecl(FD);
+
+  return true;
+}
 
 void FPRASTConsumer::HandleTranslationUnit(ASTContext &Ctx)
 {
@@ -157,13 +243,17 @@ bool FPRASTConsumer::isValidFuncDecl(FunctionDecl *FD)
   return IsValid;
 }
 
-bool FPRASTVisitor::VisitFunctionDecl(FunctionDecl *FD) {
-  FunctionDecl *CanonicalFD = FD->getCanonicalDecl();
+void FPRASTConsumer::outputTransformedSource(void)
+{
+  FileID MainFileID = SrcManager->getMainFileID();
+  const RewriteBuffer *RWBuf = TheRewriter.getRewriteBufferFor(MainFileID);
+  assert(RWBuf && "Empty RewriteBuffer!");
+  llvm::outs() << std::string(RWBuf->begin(), RWBuf->end());
+}
 
-  if (CanonicalFD == ConsumerInstance->TheFuncDecl)
-    rewriteFuncDecl(FD);
-
-  return true;
+void FPRASTConsumer::outputOriginalSource(void)
+{
+  llvm::outs() << "No Change!\n";
 }
 
 FuncParamReplacement::FuncParamReplacement(const char *TransName)
@@ -199,11 +289,16 @@ bool FuncParamReplacement::doTransformation(void)
 
   ClangInstance->getDiagnosticClient().EndSourceFile();
 
-  if (TransformationASTConsumer->transInternalError()) {
-    // FIXME:
-    // copy original source back
+  if (TransformationASTConsumer->transSuccess()) {
+    TransformationASTConsumer->outputTransformedSource();
     return true;
   }
-  return TransformationASTConsumer->notMaxInstanceError();
+  else if (TransformationASTConsumer->transInternalError()) {
+    TransformationASTConsumer->outputOriginalSource();
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
