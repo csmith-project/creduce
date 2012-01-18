d916d34d7d4308a0d00257eb33c2bab288a8b841
Yang Chen <chenyang@cs.utah.edu>
2012-01-18 20:50:49 +0000

    Added a couple of more helpers which remove var decls from vardeclgroup

--- RewriteUtils.cpp
+++ RewriteUtils.cpp
@@ -4,6 +4,7 @@
 #include "clang/Rewrite/Rewriter.h"
 #include "clang/AST/Decl.h"
 #include "clang/AST/Expr.h"
+#include "clang/AST/TypeLoc.h"
 
 // FIXME: This macro is copied from Transformation.h.
 // I don't want to include Transformation.h 
@@ -16,6 +17,34 @@
 #endif
 
 using namespace clang;
+
+SourceLocation RewriteUtils::getEndLocationFromBegin(SourceRange Range,
+                                                     Rewriter *TheRewriter)
+{
+  int LocRangeSize = TheRewriter->getRangeSize(Range);
+  if (LocRangeSize == -1)
+    return Range.getEnd();
+
+  SourceLocation StartLoc = Range.getBegin();
+
+  return StartLoc.getLocWithOffset(LocRangeSize);
+}
+
+SourceLocation RewriteUtils::getEndLocationUtil(SourceRange Range, 
+                                                char Symbol,
+                                                Rewriter *TheRewriter,
+                                                SourceManager *SrcManager)
+{
+  SourceLocation EndLoc = getEndLocationFromBegin(Range, TheRewriter);
+    
+  const char *EndBuf = SrcManager->getCharacterData(EndLoc);
+  int Offset = 0;
+  while (*EndBuf != ',') {
+    EndBuf++;
+    Offset++;
+  }
+  return EndLoc.getLocWithOffset(Offset);
+}
 
 bool RewriteUtils::removeParamFromFuncDecl(const ParmVarDecl *PV,
                                            unsigned int NumParams,
@@ -159,3 +188,66 @@ bool RewriteUtils::removeArgFromCallExpr(CallExpr *CallE,
   return true;
 }
 
+bool RewriteUtils::removeVarFromDeclStmt(DeclStmt *DS,
+                                         VarDecl *VD,
+                                         Decl *PrevDecl,
+                                         bool IsFirstDecl,
+                                         Rewriter *TheRewriter,
+                                         SourceManager *SrcManager)
+{
+  SourceRange StmtRange = DS->getSourceRange();
+
+  // VD is the the only declaration, so it is safe to remove the entire stmt
+  if (DS->isSingleDecl()) {
+    return !(TheRewriter->RemoveText(StmtRange));
+  }
+
+  SourceRange VarRange = VD->getSourceRange();
+
+  // VD is the first declaration in a declaration group.
+  // We keep the leading type string
+  if (IsFirstDecl) {
+    TypeLoc VarTypeLoc = VD->getTypeSourceInfo()->getTypeLoc();
+
+    TypeLoc NextTL = VarTypeLoc.getNextTypeLoc();
+    while (!NextTL.isNull()) {
+      VarTypeLoc = NextTL;
+      NextTL = NextTL.getNextTypeLoc();
+    }
+
+    SourceRange TypeLocRange = VarTypeLoc.getSourceRange();
+    SourceLocation NewStartLoc = 
+      getEndLocationFromBegin(TypeLocRange, TheRewriter);
+
+    SourceLocation NewEndLoc = 
+      getEndLocationUtil(VarRange, ',', TheRewriter, SrcManager);
+    
+    return 
+      !(TheRewriter->RemoveText(SourceRange(NewStartLoc, NewEndLoc)));
+  }
+
+  TransAssert(PrevDecl && "PrevDecl cannot be NULL!");
+  SourceLocation VarEndLoc = VarRange.getEnd();
+  SourceRange PrevDeclRange = PrevDecl->getSourceRange();
+  SourceLocation PrevDeclEndLoc = 
+    getEndLocationUtil(PrevDeclRange, ',', TheRewriter, SrcManager);
+
+  return !(TheRewriter->RemoveText(SourceRange(PrevDeclEndLoc, VarEndLoc)));
+}
+
+bool RewriteUtils::getExprString(const Expr *E, 
+                                 std::string &ES,
+                                 Rewriter *TheRewriter,
+                                 SourceManager *SrcManager)
+{
+  SourceRange ExprRange = E->getSourceRange();
+   
+  int RangeSize = TheRewriter->getRangeSize(ExprRange);
+  if (RangeSize == -1)
+    return false;
+
+  SourceLocation StartLoc = ExprRange.getBegin();
+  const char *StartBuf = SrcManager->getCharacterData(StartLoc);
+  ES.assign(StartBuf, RangeSize);
+  return true;
+}
--- RewriteUtils.h
+++ RewriteUtils.h
@@ -1,11 +1,18 @@
 #ifndef REWRITE_UTILS_H
 #define REWRITE_UTILS_h
 
+#include <string>
+#include "clang/Basic/SourceLocation.h"
+
 namespace clang {
   class ParmVarDecl;
+  class VarDecl;
+  class Decl;
+  class DeclStmt;
   class Rewriter;
   class SourceManager;
   class CallExpr;
+  class Expr;
 }
 
 class RewriteUtils {
@@ -21,8 +28,26 @@ public:
                                     clang::Rewriter *TheRewriter,
                                     clang::SourceManager *SrcManager);
                                     
+  static bool removeVarFromDeclStmt(clang::DeclStmt *DS,
+                                    clang::VarDecl *VD,
+                                    clang::Decl *PrevDecl,
+                                    bool IsFirstDecl,
+                                    clang::Rewriter *TheRewriter,
+                                    clang::SourceManager *SrcManager);
                                  
+  static bool getExprString(const clang::Expr *E, 
+                            std::string &ES,
+                            clang::Rewriter *TheRewriter,
+                            clang::SourceManager *SrcManager);
 private:
+
+  static clang::SourceLocation getEndLocationFromBegin
+    (clang::SourceRange Range, clang::Rewriter *TheRewriter);
+
+  static clang::SourceLocation getEndLocationUtil(clang::SourceRange Range,
+                                           char Symbol, 
+                                           clang::Rewriter *TheRewriter,
+                                           clang::SourceManager *SrcManager);
 
   RewriteUtils(void);
 
