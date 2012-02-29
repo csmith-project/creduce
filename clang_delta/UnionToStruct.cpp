2b8df54dc83614e613ffe66f23dfc6ca75a043c3
Yang Chen <chenyang@cs.utah.edu>
2012-02-29 04:26:31 -0700

    A new pass union-to-struct is getting close to work
    Still has some bugs, will fix them soon.

---
+++ clang_delta/UnionToStruct.cpp
@@ -0,0 +1,378 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (c) 2012 The University of Utah
+// All rights reserved.
+//
+// This file is distributed under the University of Illinois Open Source
+// License.  See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "UnionToStruct.h"
+
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Basic/SourceManager.h"
+
+#include "TransformationManager.h"
+
+using namespace clang;
+using namespace llvm;
+
+static const char *DescriptionMsg =
+"Change a union declaration to a struct declaration. \
+For a union declaration which has only scalar fields, i.e., fields \
+with integer or pointer types, the transformation will set up an \
+appropriate initializer for a variable which is declared with a union \
+type under transformed: \n\
+  * if originally a pointer field gets initialization value, then \
+other pointer fields with the same type will get the same initialization \
+value, and all other fields will be initialized to 0; \n\
+  * if originally a integer field gets initialization value, then all \
+other integer fields will be initialized to the same value, while all \
+pointer fields will have 0. \n\
+\n\
+For other cases, all initializers will be dropped. (Note that this could \n\
+bring in uninitialized local variables.) \n";
+
+static RegisterTransformation<UnionToStruct>
+         Trans("union-to-struct", DescriptionMsg);
+
+class UnionToStructCollectionVisitor : public 
+  RecursiveASTVisitor<UnionToStructCollectionVisitor> {
+
+public:
+  explicit UnionToStructCollectionVisitor(UnionToStruct *Instance)
+    : ConsumerInstance(Instance)
+  { }
+
+  bool VisitVarDecl(VarDecl *VD);
+
+  bool VisitFieldDecl(FieldDecl *FD);
+
+  bool VisitRecordDecl(RecordDecl *RD);
+
+  bool VisitDeclStmt(DeclStmt *DS);
+
+private:
+
+  UnionToStruct *ConsumerInstance;
+};
+
+bool UnionToStructCollectionVisitor::VisitVarDecl(VarDecl *VD)
+{
+  ConsumerInstance->addOneDeclarator(VD);
+  return true;
+}
+
+bool UnionToStructCollectionVisitor::VisitFieldDecl(FieldDecl *FD)
+{
+  ConsumerInstance->addOneDeclarator(FD);
+  return true;
+}
+
+bool UnionToStructCollectionVisitor::VisitRecordDecl(RecordDecl *RD)
+{
+  if (RD->isUnion())
+    ConsumerInstance->addOneRecord(RD);
+
+  return true;
+}
+
+bool UnionToStructCollectionVisitor::VisitDeclStmt(DeclStmt *DS)
+{
+  for (DeclStmt::decl_iterator I = DS->decl_begin(),
+       E = DS->decl_end(); I != E; ++I) {
+    VarDecl *CurrVD = dyn_cast<VarDecl>(*I);
+    if (CurrVD)
+      ConsumerInstance->VarToDeclStmt[CurrVD] = DS;
+  }
+  return true;
+}
+
+void UnionToStruct::Initialize(ASTContext &context) 
+{
+  Transformation::Initialize(context);
+  CollectionVisitor = new UnionToStructCollectionVisitor(this);
+}
+
+void UnionToStruct::HandleTopLevelDecl(DeclGroupRef D) 
+{
+  for (DeclGroupRef::iterator I = D.begin(), E = D.end(); I != E; ++I) {
+    VarDecl *VD = dyn_cast<VarDecl>(*I);
+    if (VD)
+      VarToDeclGroup[VD] = D;
+
+    CollectionVisitor->TraverseDecl(*I);
+  }
+}
+ 
+void UnionToStruct::HandleTranslationUnit(ASTContext &Ctx)
+{
+  doAnalysis();
+  if (QueryInstanceOnly)
+    return;
+
+  if (TransformationCounter > ValidInstanceNum) {
+    TransError = TransMaxInstanceError;
+    return;
+  }
+
+  TransAssert(TheRecordDecl && "Cannot have NULL TheRecordDecl!");
+  TransAssert(TheDeclaratorSet && "Cannot have NULL TheDeclaratorSet!");
+
+  rewriteRecordDecls();
+  rewriteDeclarators();
+
+  Ctx.getDiagnostics().setSuppressAllDiagnostics(false);
+
+  if (Ctx.getDiagnostics().hasErrorOccurred() ||
+      Ctx.getDiagnostics().hasFatalErrorOccurred())
+    TransError = TransInternalError;
+}
+
+bool UnionToStruct::isValidRecordDecl(const RecordDecl *RD)
+{
+  if (!RD->isCompleteDefinition())
+    return false;
+
+  if (RD->isAnonymousStructOrUnion())
+    return false;
+
+  for (RecordDecl::field_iterator I = RD->field_begin(),
+       E = RD->field_end(); I != E; ++I) {
+    const FieldDecl *FD = (*I);
+    const Type *FDTy = FD->getType().getTypePtr();
+    if (!FDTy->isScalarType())
+      return false;
+  }
+  return true;
+}
+
+void UnionToStruct::doAnalysis(void)
+{
+  for (RecordDeclToDeclaratorDeclMap::iterator I = RecordToDeclarator.begin(),
+       E = RecordToDeclarator.end(); I != E; ++I) {
+    ValidInstanceNum++;
+    if (ValidInstanceNum == TransformationCounter) {
+      TheRecordDecl = ((*I).first)->getDefinition();
+      TheDeclaratorSet = (*I).second;
+    }
+  }
+}
+
+void UnionToStruct::rewriteOneRecordDecl(const RecordDecl *RD)
+{
+  TransAssert(RD && "NULL RecordDecl!");
+  RewriteHelper->replaceUnionWithStruct(RD);
+}
+
+void UnionToStruct::rewriteRecordDecls(void)
+{
+  const RecordDecl *RD = 
+    dyn_cast<RecordDecl>(TheRecordDecl->getFirstDeclaration());
+  TransAssert(RD && "NULL RecordDecl!");
+  for(RecordDecl::redecl_iterator I = RD->redecls_begin(),
+      E = RD->redecls_end(); I != E; ++I) {
+    rewriteOneRecordDecl(dyn_cast<RecordDecl>(*I));
+  }
+}
+
+void UnionToStruct::rewriteOneFieldDecl(const FieldDecl *FD)
+{
+  const DeclContext *Ctx = TheRecordDecl->getLexicalParent();
+  const RecordDecl *TmpRD = dyn_cast<RecordDecl>(Ctx);
+  // Skip the case where we have:
+  // struct {
+  //  union U {
+  //    int f;
+  //  } f1, f2;
+  // };
+  // union U is handled by rewriteOneRecordDecl;
+  if (!TmpRD)
+    RewriteHelper->replaceUnionWithStruct(FD);
+}
+
+void UnionToStruct::getInitStrWithPointerType(const Expr *Exp, std::string &Str)
+{
+  std::string ExprStr("");
+  RewriteHelper->getExprString(Exp, ExprStr);
+
+  RecordDecl::field_iterator I = TheRecordDecl->field_begin();
+  RecordDecl::field_iterator E = TheRecordDecl->field_end();
+  TransAssert((I != E) && "Empty RecordDecl!");
+  Str = ExprStr;
+  ++I;
+
+  if (I == E)
+    return;
+
+  QualType ETy = Exp->getType().getCanonicalType();
+  for (; I != E; ++I) {
+    Str += ",";
+    const FieldDecl *FD = (*I);
+    QualType FTy = FD->getType().getCanonicalType();
+    if (ETy == FTy)
+      Str += ExprStr;
+    else
+      Str += "0";
+  }
+}
+
+void UnionToStruct::getInitStrWithNonPointerType(const Expr *Exp, 
+                                                 std::string &Str)
+{
+  std::string ExprStr("");
+  RewriteHelper->getExprString(Exp, ExprStr);
+
+  RecordDecl::field_iterator I = TheRecordDecl->field_begin();
+  RecordDecl::field_iterator E = TheRecordDecl->field_end();
+  TransAssert((I != E) && "Empty RecordDecl!");
+  Str = ExprStr;
+  ++I;
+
+  if (I == E)
+    return;
+
+  for (; I != E; ++I) {
+    Str += ",";
+    const FieldDecl *FD = (*I);
+    const Type *FTy = FD->getType().getTypePtr();
+    if (FTy->isPointerType())
+      Str += "0";
+    else
+      Str += ExprStr;
+  }
+}
+
+bool UnionToStruct::isTheFirstDecl(const VarDecl *VD,
+                                   DeclGroupRef DGR)
+{
+  if (DGR.isSingleDecl())
+    return true;
+
+  DeclGroupRef::const_iterator I = DGR.begin();
+  const VarDecl *FirstVD = dyn_cast<VarDecl>(*I);
+  if (!FirstVD)
+    return false;
+
+  return (VD == FirstVD);
+}
+
+void UnionToStruct::rewriteOneVarDecl(const VarDecl *VD)
+{
+  if (dyn_cast<ParmVarDecl>(VD)) {
+    RewriteHelper->replaceUnionWithStruct(VD);
+    return; 
+  }
+
+  const DeclStmt *DS = VarToDeclStmt[VD];
+  DeclGroupRef DGR;
+  if (DS)
+    DGR = DS->getDeclGroup();
+  else
+    DGR = VarToDeclGroup[VD];
+
+  TransAssert(!DGR.isNull() && "Bad DeclRefGroup!");
+
+  // If the first decl is RecordDecl, it will be handled
+  // by rewriteOneRecordDecl.
+  if (isTheFirstDecl(VD, DGR))
+    RewriteHelper->replaceUnionWithStruct(VD);
+
+  const Type *VDTy = VD->getType().getTypePtr();
+  if (!VD->hasInit() || !VDTy->isUnionType())
+    return;
+
+  if (!isValidRecordDecl(TheRecordDecl)) {
+    RewriteHelper->removeVarInitExpr(VD);
+    return;
+  }
+
+  const Expr *IE = VD->getInit();
+  const InitListExpr *ILE = dyn_cast<InitListExpr>(IE);
+  TransAssert(ILE && "Bad InitListExpr!");
+
+  if (ILE->getNumInits() != 1) {
+    RewriteHelper->removeVarInitExpr(VD);
+    return;
+  }
+
+  const Expr *FirstE = ILE->getInit(0);
+  const Type *ExprTy = FirstE->getType().getTypePtr();
+  std::string NewInitStr;
+
+  if (ExprTy->isPointerType()) {
+    getInitStrWithPointerType(FirstE, NewInitStr);
+  }
+  else {
+    getInitStrWithNonPointerType(FirstE, NewInitStr);
+  }
+
+  RewriteHelper->replaceExpr(FirstE, NewInitStr);
+}
+
+void UnionToStruct::rewriteDeclarators(void)
+{
+  for (DeclaratorDeclSet::const_iterator I = TheDeclaratorSet->begin(),
+       E = TheDeclaratorSet->end(); I != E; ++I) {
+    const FieldDecl *FD = dyn_cast<FieldDecl>(*I);
+    if (FD) {
+      rewriteOneFieldDecl(FD);
+      continue;
+    }
+
+    const VarDecl *VD = dyn_cast<VarDecl>(*I);
+    TransAssert(VD && "Invalid Declarator kind!");
+    rewriteOneVarDecl(VD);
+  }
+}
+
+void UnionToStruct::addOneDeclarator(const DeclaratorDecl *DD)
+{
+  const Type *T = DD->getType().getTypePtr();
+  const ArrayType *ArrayTy = dyn_cast<ArrayType>(T);
+  if (ArrayTy)
+    T = getArrayBaseElemType(ArrayTy);
+
+  if (T->isPointerType())
+    T = getBasePointerElemType(T);
+
+  if (!T->isUnionType())
+    return;
+
+  const RecordType *RDTy = T->getAsUnionType();
+  TransAssert(RDTy && "Bad RecordType!");
+
+  const RecordDecl *RD = RDTy->getDecl(); 
+  const RecordDecl *CanonicalRD = 
+    dyn_cast<RecordDecl>(RD->getCanonicalDecl());
+  TransAssert(CanonicalRD && "NULL CanonicalRD!");
+  DeclaratorDeclSet *DDSet = RecordToDeclarator[CanonicalRD];
+  TransAssert(DDSet && "Cannot find VarDeclSet for a given RecordDecl!");
+  DDSet->insert(DD);
+}
+
+void UnionToStruct::addOneRecord(const RecordDecl *RD)
+{
+  const RecordDecl *CanonicalRD = 
+    dyn_cast<RecordDecl>(RD->getCanonicalDecl());
+  TransAssert(CanonicalRD && "NULL CanonicalRD!");
+  if (RecordToDeclarator[CanonicalRD])
+    return;
+
+  DeclaratorDeclSet *DDSet = new DeclaratorDeclSet();
+  TransAssert(DDSet && "Member allocation failure!");
+  RecordToDeclarator[CanonicalRD] = DDSet;
+}
+
+UnionToStruct::~UnionToStruct(void)
+{
+  if (CollectionVisitor)
+    delete CollectionVisitor;
+
+  for (RecordDeclToDeclaratorDeclMap::iterator I = RecordToDeclarator.begin(),
+       E = RecordToDeclarator.end(); I != E; ++I) {
+    delete (*I).second;
+  }
+}
+
---
+++ clang_delta/UnionToStruct.h
@@ -0,0 +1,108 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (c) 2012 The University of Utah
+// All rights reserved.
+//
+// This file is distributed under the University of Illinois Open Source
+// License.  See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef UNION_TO_STRUCT_H
+#define UNION_TO_STRUCT_H
+
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/SmallPtrSet.h"
+#include "Transformation.h"
+
+namespace clang {
+  class DeclGroupRef;
+  class ASTContext;
+  class DeclaratorDecl;
+  class VarDecl;
+  class RecordDecl;
+  class DeclStmt;
+  class FieldDecl;
+}
+
+class UnionToStructCollectionVisitor;
+
+class UnionToStruct : public Transformation {
+friend class UnionToStructCollectionVisitor;
+
+public:
+
+  UnionToStruct(const char *TransName, const char *Desc)
+    : Transformation(TransName, Desc),
+      CollectionVisitor(NULL),
+      TheRecordDecl(NULL),
+      TheDeclaratorSet(NULL)
+  { }
+
+  ~UnionToStruct(void);
+
+private:
+  
+  typedef llvm::SmallPtrSet<const clang::DeclaratorDecl *, 10> 
+    DeclaratorDeclSet;
+
+  typedef llvm::DenseMap<const clang::VarDecl *, const clang::DeclStmt *> 
+            VarToDeclStmtMap;
+
+  typedef llvm::DenseMap<const clang::VarDecl *, clang::DeclGroupRef> 
+            VarToDeclGroupMap;
+
+  typedef llvm::DenseMap<const clang::RecordDecl *, DeclaratorDeclSet *> 
+            RecordDeclToDeclaratorDeclMap;
+
+  virtual void Initialize(clang::ASTContext &context);
+
+  virtual void HandleTopLevelDecl(clang::DeclGroupRef D);
+
+  virtual void HandleTranslationUnit(clang::ASTContext &Ctx);
+
+  void addOneDeclarator(const clang::DeclaratorDecl *VD);
+
+  void addOneRecord(const clang::RecordDecl *RD);
+
+  void doAnalysis(void);
+
+  bool isValidRecordDecl(const clang::RecordDecl *RD);
+
+  void rewriteRecordDecls(void);
+
+  void rewriteDeclarators(void);
+
+  void rewriteOneRecordDecl(const clang::RecordDecl *RD);
+
+  void rewriteOneVarDecl(const clang::VarDecl *VD);
+
+  void rewriteOneFieldDecl(const clang::FieldDecl *FD);
+
+  void getInitStrWithPointerType(const clang::Expr *E, std::string &Str);
+
+  void getInitStrWithNonPointerType(const clang::Expr *E, std::string &Str);
+
+  bool isTheFirstDecl(const clang::VarDecl *VD, 
+                      clang::DeclGroupRef DGR);
+
+  VarToDeclStmtMap VarToDeclStmt;
+
+  VarToDeclGroupMap VarToDeclGroup;
+
+  RecordDeclToDeclaratorDeclMap RecordToDeclarator;
+
+  UnionToStructCollectionVisitor *CollectionVisitor;
+
+  const clang::RecordDecl *TheRecordDecl;
+
+  DeclaratorDeclSet *TheDeclaratorSet;
+
+  // Unimplemented
+  UnionToStruct(void);
+
+  UnionToStruct(const UnionToStruct &);
+
+  void operator=(const UnionToStruct &);
+};
+#endif
