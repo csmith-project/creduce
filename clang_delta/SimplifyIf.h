79d48f62901dc316918aec8fb45044fe28cca2bc
Yang Chen <chenyang@cs.utah.edu>
2012-03-01 14:57:35 -0700

    simplify-if pass looks good now
    This pass simplifies an if-else statement. It transforms the following code:
      if (guard1)
      {... }
      else if (guard2)
      else
      {...}
    
    to
    
      guard1;
      {... }
      if (guard2)
      else
      {...}
    if there is no else-if left, the last else keyword will be removed.

---
+++ clang_delta/SimplifyIf.h
@@ -0,0 +1,63 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (c) 2012 The University of Utah
+// All rights reserved.
+//
+// This file is distributed under the University of Illinois Open Source
+// License.  See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SIMPLIFY_IF_H
+#define SIMPLIFY_IF_H
+
+#include <string>
+#include "Transformation.h"
+
+namespace clang {
+  class DeclGroupRef;
+  class ASTContext;
+  class Stmt;
+  class IfStmt;
+}
+
+class SimplifyIfCollectionVisitor;
+
+class SimplifyIf : public Transformation {
+friend class SimplifyIfCollectionVisitor;
+
+public:
+
+  SimplifyIf(const char *TransName, const char *Desc)
+    : Transformation(TransName, Desc),
+      CollectionVisitor(NULL),
+      TheIfStmt(NULL),
+      NeedParen(false)
+  { }
+
+  ~SimplifyIf(void);
+
+private:
+  
+  virtual void Initialize(clang::ASTContext &context);
+
+  virtual void HandleTopLevelDecl(clang::DeclGroupRef D);
+
+  virtual void HandleTranslationUnit(clang::ASTContext &Ctx);
+
+  void simplifyIfStmt(void);
+
+  SimplifyIfCollectionVisitor *CollectionVisitor;
+
+  clang::IfStmt *TheIfStmt;
+
+  bool NeedParen;
+
+  // Unimplemented
+  SimplifyIf(void);
+
+  SimplifyIf(const SimplifyIf &);
+
+  void operator=(const SimplifyIf &);
+};
+#endif
