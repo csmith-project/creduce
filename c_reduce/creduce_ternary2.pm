b3ea69c222179999514994c1a2dedf74636ebf0e
John Regehr <regehr@cs.utah.edu>
2012-03-01 16:29:57 -0700

    just some work

---
+++ c_reduce/creduce_ternary2.pm
@@ -0,0 +1,50 @@
+#!/usr/bin/perl -w
+
+package creduce_ternary;
+
+use Regexp::Common;
+use re 'eval';
+use Exporter::Lite;
+
+@EXPORT      = qw(ternary1 ternary2);
+
+use strict;
+
+use creduce_regexes;
+use creduce_utils;
+
+sub ternary1 ($$) {
+    (my $cfile, my $index) = @_;
+
+    my $string = read_file ($cfile);
+    my $string2 = $string;
+
+    $replace_cont = -1;
+    $string2 =~ s/(?<all>(?<del1>$borderorspc)(?<a>$varnumexp)\s*\?\s*(?<b>$varnumexp)\s*:\s*(?<c>$varnumexp)(?<del2>$borderorspc))/replace_aux($index,$+{all},$+{del1}.$+{b}.$+{del2})/eg;
+
+    if ($string ne $string2) {
+	write_file ($cfile, $string2);
+	return $SUCCESS;
+    } else {
+	return $STOP;
+    }
+}
+
+sub ternary2 ($$) {
+    (my $cfile, my $index) = @_;
+
+    my $string = read_file ($cfile);
+    my $string2 = $string;
+
+    $replace_cont = -1;
+    $string2 =~ s/(?<all>(?<del1>$borderorspc)(?<a>$varnumexp)\s*\?\s*(?<b>$varnumexp)\s*:\s*(?<c>$varnumexp)(?<del2>$borderorspc))/replace_aux($index,$+{all},$+{del1}.$+{c}.$+{del2})/eg;
+
+    if ($string ne $string2) {
+	write_file ($cfile, $string2);
+	return $SUCCESS;
+    } else {
+	return $STOP;
+    }
+}
+
+1;
