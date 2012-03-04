dfb6f47912f82b7bdb720bc35d1edc654cb03003
John Regehr <regehr@cs.utah.edu>
2012-03-04 11:12:28 -0700

    oops, move code to its right place

--- c_reduce/c_reduce.pl
+++ c_reduce/c_reduce.pl
@@ -27,54 +27,6 @@ my $QUIET = 0;
 
 ######################################################################
 
-package pass_crc;
-
-use strict;
-
-use creduce_regexes;
-use creduce_utils;
-
-sub check_prereqs () {
-    return 1;
-}
-
-my $index;
-
-sub init () {
-    $index = 0;
-}
-
-sub advance () {
-    $index++;
-}
-
-sub junk ($) {
-    (my $list) = @_;
-    my @stuff = split /,/, $list;
-    my $var = $stuff[0];
-    return "printf (\"%d\\n\", (int)$var)";
-}
-
-sub transform ($$) {
-    (my $cfile, my $which) = @_;
-
-    my $prog = read_file ($cfile);
-    my $prog2 = $prog;
-
-    $replace_cont = -1;
-    $prog2 =~ s/(?<all>transparent_crc\s*\((?<list>.*?)\))/replace_aux($index,$+{all},junk($+{list}))/egs;
-
-    if ($prog ne $prog2) {
-	write_file ($cfile, $prog2);
-	return $SUCCESS;
-    } else {
-	return $STOP;
-    }
-}
-
-package main;
-
-
 my $orig_file_size;
 
 sub print_pct ($) {
