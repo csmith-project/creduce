0f2fb1f0b5c548f5290b44d7eb62063d798093e6
Yang Chen <chenyang@cs.utah.edu>
2012-03-07 22:57:25 -0700

    Added another trivial pass move-global-var
    Move global vars above all function declarations except printf.
    Also try to move the declaration of printf to the top of the code
    if it exists and is not at the top.

---
+++ clang_delta/MoveGlobalVar.cpp
@@ -0,0 +1,140 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (c) 2012 The University of Utah
+// All rights reserved.
+//
+// This file is distributed under the University of Illinois Open Source
+// License.  See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "MoveGlobalVar.h"
+
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Basic/SourceManager.h"
+
+#include "TransformationManager.h"
+
+using namespace clang;
+
+static const char *DescriptionMsg =
+"Try to move global var/struct/union declarations above all function \
+declarations except printf. Also move the declaration of printf to the top of \
+the reduce code if it exists and is not at the top of the code.\n";
+
+static RegisterTransformation<MoveGlobalVar>
+         Trans("move-global-var", DescriptionMsg);
+
+void MoveGlobalVar::Initialize(ASTContext &context) 
+{
+  Transformation::Initialize(context);
+}
+
+bool MoveGlobalVar::isSpecialDecl(const std::string &Name)
+{
+  return ((Name == "__va_list_tag") ||
+          (Name == "__builtin_va_list"));
+}
+
+void MoveGlobalVar::HandleTopLevelDecl(DeclGroupRef D) 
+{
+  DeclGroupRef::iterator I = D.begin();
+  TransAssert((I != D.end()) && "Bad DeclGroupRef!");
+
+  const NamedDecl *ND = dyn_cast<NamedDecl>(*I);
+  if (!TheFirstDecl && ND && isSpecialDecl(ND->getNameAsString()))
+    return;
+
+  FunctionDecl *FD = dyn_cast<FunctionDecl>(*I);
+  if (FD) {
+    handleFunctionDecl(FD); 
+  }
+  else {
+    handleOtherDecl(D);
+  }
+
+  if (!TheFirstDecl)
+    TheFirstDecl = (*I);
+}
+
+void MoveGlobalVar::HandleTranslationUnit(ASTContext &Ctx)
+{
+  if (QueryInstanceOnly)
+    return;
+
+  if (TransformationCounter > ValidInstanceNum) {
+    TransError = TransMaxInstanceError;
+    return;
+  }
+
+  Ctx.getDiagnostics().setSuppressAllDiagnostics(false);
+
+  if (ThePrintfDecl)
+    liftPrintfDecl();
+  else
+    liftOtherDecl();
+
+  if (Ctx.getDiagnostics().hasErrorOccurred() ||
+      Ctx.getDiagnostics().hasFatalErrorOccurred())
+    TransError = TransInternalError;
+}
+
+void MoveGlobalVar::handleFunctionDecl(const FunctionDecl *FD)
+{
+  if (FD->getNameAsString() == "printf") {
+    if (ThePrintfDecl || !TheFirstDecl || FD->isThisDeclarationADefinition())
+      return;
+
+    ValidInstanceNum++;
+    if (TransformationCounter == ValidInstanceNum)
+      ThePrintfDecl = FD;
+  }
+  else if (!TheFirstFunctionDecl) {
+    TheFirstFunctionDecl = FD;
+  }
+}
+
+void MoveGlobalVar::handleOtherDecl(DeclGroupRef DGR)
+{
+  if (!TheFirstFunctionDecl)
+    return;
+
+  ValidInstanceNum++;
+  if (TransformationCounter == ValidInstanceNum)
+    TheDGRPointer = DGR.getAsOpaquePtr();
+}
+
+void MoveGlobalVar::liftPrintfDecl(void)
+{
+  TransAssert(ThePrintfDecl && TheFirstDecl && 
+              (ThePrintfDecl != TheFirstDecl) && "Invalid printf decl!");
+
+  std::string PrintfDeclStr;
+  RewriteHelper->getFunctionDeclStrAndRemove(ThePrintfDecl, PrintfDeclStr);
+
+  SourceRange DeclRange = TheFirstDecl->getSourceRange();
+  SourceLocation StartLoc = DeclRange.getBegin();
+  PrintfDeclStr += ";\n";
+  TheRewriter.InsertTextBefore(StartLoc, PrintfDeclStr);
+}
+
+void MoveGlobalVar::liftOtherDecl(void)
+{
+  TransAssert(TheDGRPointer && "NULL DGR pointer!");
+  TransAssert(TheFirstFunctionDecl && "NULL First Decl!");
+  DeclGroupRef DGR = DeclGroupRef::getFromOpaquePtr(TheDGRPointer);
+
+  std::string DGRStr;
+  RewriteHelper->getEntireDeclGroupStrAndRemove(DGR, DGRStr);
+
+  SourceRange DeclRange = TheFirstFunctionDecl->getSourceRange();
+  SourceLocation StartLoc = DeclRange.getBegin();
+  DGRStr += ";\n";
+  TheRewriter.InsertTextBefore(StartLoc, DGRStr);
+}
+
+MoveGlobalVar::~MoveGlobalVar(void)
+{
+  // Nothing to do
+}
+
