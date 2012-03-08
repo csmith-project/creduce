d7d93b3ae32e16ada493c6f972be946ed175ef37
Yang Chen <chenyang@cs.utah.edu>
2012-03-08 13:54:20 -0700

    A quick hack to a new pass
    unify-function-decl - If a function is declared as static,
    make its definition as static as well, and remove the declaration

---
+++ clang_delta/UnifyFunctionDecl.cpp
@@ -0,0 +1,150 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (c) 2012 The University of Utah
+// All rights reserved.
+//
+// This file is distributed under the University of Illinois Open Source
+// License.  See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "UnifyFunctionDecl.h"
+
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Basic/SourceManager.h"
+
+#include "TransformationManager.h"
+
+using namespace clang;
+
+static const char *DescriptionMsg =
+"If a function is declared as static, make its definition \
+as static as well, and remove the declaration. \n";
+
+static RegisterTransformation<UnifyFunctionDecl>
+         Trans("unify-function-decl", DescriptionMsg);
+
+void UnifyFunctionDecl::Initialize(ASTContext &context) 
+{
+  Transformation::Initialize(context);
+}
+
+void UnifyFunctionDecl::HandleTopLevelDecl(DeclGroupRef D) 
+{
+  for (DeclGroupRef::iterator I = D.begin(), E = D.end(); I != E; ++I) {
+    const FunctionDecl *FD = dyn_cast<FunctionDecl>(*I);
+    if (!FD)
+      return;    
+
+    if (!FD->hasBody())
+      return;
+
+    const FunctionDecl *CanonicalFD = FD->getCanonicalDecl();
+    if (VisitedFunctionDecls.count(CanonicalFD))
+      return;
+
+    VisitedFunctionDecls.insert(CanonicalFD);
+  }
+}
+
+void UnifyFunctionDecl::HandleTranslationUnit(ASTContext &Ctx)
+{
+  doAnalysis();
+
+  if (QueryInstanceOnly)
+    return;
+
+  if (TransformationCounter > ValidInstanceNum) {
+    TransError = TransMaxInstanceError;
+    return;
+  }
+
+  Ctx.getDiagnostics().setSuppressAllDiagnostics(false);
+
+  TransAssert(TheFunctionDecl && "NULL TheFunctionDecl!");
+  TransAssert(TheFunctionDef && "NULL TheFunctionDef!");
+
+  doRewriting();
+
+  if (Ctx.getDiagnostics().hasErrorOccurred() ||
+      Ctx.getDiagnostics().hasFatalErrorOccurred())
+    TransError = TransInternalError;
+}
+
+void UnifyFunctionDecl::doAnalysis(void)
+{
+  for (FunctionDeclSet::iterator I = VisitedFunctionDecls.begin(),
+       E = VisitedFunctionDecls.end(); I != E; ++I) {
+
+    const FunctionDecl *FDDef, *FDDecl;
+    const FunctionDecl *CanonicalFD = (*I);
+    const FunctionDecl *FirstFD = CanonicalFD->getFirstDeclaration();
+
+    FunctionDecl::redecl_iterator RI = FirstFD->redecls_begin();
+    ++RI;
+    if (RI == FirstFD->redecls_end())
+      continue;
+
+    const FunctionDecl *SecondFD = (*RI);
+    if (FirstFD->isThisDeclarationADefinition()) {
+      FDDef = FirstFD;
+      TransAssert(!SecondFD->isThisDeclarationADefinition() &&
+                  "Duplicated Definition!");
+      FDDecl = SecondFD;
+    }
+    else if (SecondFD->isThisDeclarationADefinition()) {
+      FDDef = SecondFD;
+      TransAssert(!FirstFD->isThisDeclarationADefinition() &&
+                  "Duplicated Definition!");
+      FDDecl = FirstFD;
+    }
+    else {
+      TransAssert(0 && "No Function Definition!");
+    }
+
+    if (!hasStaticKeyword(FDDecl) || hasStaticKeyword(FDDef))
+      continue;
+
+    ValidInstanceNum++;
+    if (TransformationCounter == ValidInstanceNum) {
+      TheFunctionDef = FDDef;
+      TheFunctionDecl = FDDecl;
+    }
+  }
+}
+
+bool UnifyFunctionDecl::hasStaticKeyword(const FunctionDecl *FD)
+{
+  SourceRange FDRange = FD->getSourceRange();
+  SourceLocation StartLoc = FDRange.getBegin();
+  SourceLocation EndLoc = FD->getLocation();
+
+  const char *StartBuf = SrcManager->getCharacterData(StartLoc);
+  const char *EndBuf = SrcManager->getCharacterData(EndLoc);
+  std::string Str;
+  if (StartBuf == EndBuf)
+    return false;
+
+  TransAssert(StartBuf < EndBuf);
+  size_t Off = EndBuf - StartBuf;
+  Str.assign(StartBuf, Off);
+
+  size_t Pos = Str.find("static");
+  return (Pos != std::string::npos);
+}
+
+void UnifyFunctionDecl::doRewriting(void)
+{
+  SourceRange FDDefRange = TheFunctionDef->getSourceRange();
+  SourceLocation StartLoc = FDDefRange.getBegin();
+  TheRewriter.InsertTextBefore(StartLoc, "static ");
+
+  std::string TmpStr;
+  RewriteHelper->getFunctionDeclStrAndRemove(TheFunctionDecl, TmpStr);
+}
+
+UnifyFunctionDecl::~UnifyFunctionDecl(void)
+{
+  // Nothing to do
+}
+
---
+++ clang_delta/UnifyFunctionDecl.h
@@ -0,0 +1,66 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (c) 2012 The University of Utah
+// All rights reserved.
+//
+// This file is distributed under the University of Illinois Open Source
+// License.  See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef UNIFY_FUNCTION_DECL_H
+#define UNIFY_FUNCTION_DECL_H
+
+#include <string>
+#include "llvm/ADT/SmallPtrSet.h"
+#include "Transformation.h"
+
+namespace clang {
+  class DeclGroupRef;
+  class ASTContext;
+  class FunctionDecl;
+}
+
+class UnifyFunctionDecl : public Transformation {
+
+public:
+
+  UnifyFunctionDecl(const char *TransName, const char *Desc)
+    : Transformation(TransName, Desc),
+      TheFunctionDecl(NULL),
+      TheFunctionDef(NULL)
+  { }
+
+  ~UnifyFunctionDecl(void);
+
+private:
+  
+  typedef llvm::SmallPtrSet<const clang::FunctionDecl *, 10>
+            FunctionDeclSet;
+
+  virtual void Initialize(clang::ASTContext &context);
+
+  virtual void HandleTopLevelDecl(clang::DeclGroupRef D);
+
+  virtual void HandleTranslationUnit(clang::ASTContext &Ctx);
+
+  void doAnalysis(void);
+
+  void doRewriting(void);
+
+  bool hasStaticKeyword(const clang::FunctionDecl *FD);
+
+  FunctionDeclSet VisitedFunctionDecls;
+
+  const clang::FunctionDecl *TheFunctionDecl;
+
+  const clang::FunctionDecl *TheFunctionDef;
+
+  // Unimplemented
+  UnifyFunctionDecl(void);
+
+  UnifyFunctionDecl(const UnifyFunctionDecl &);
+
+  void operator=(const UnifyFunctionDecl &);
+};
+#endif
