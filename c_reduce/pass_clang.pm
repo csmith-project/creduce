c334196041b132d4c19baf9d858f1a9461d25c61
John Regehr <regehr@cs.utah.edu>
2012-04-10 14:52:20 -0600

    add untested logic for saving a copy of a file that makes clang_delta crash; it will get tested next time that tool crashes

--- c_reduce/pass_clang.pm
+++ c_reduce/pass_clang.pm
@@ -30,12 +30,25 @@ sub transform ($$$) {
     (my $cfile, my $which, my $state) = @_;
     my $index = ${$state};
     my $tmpfile = POSIX::tmpnam();
-    my $cmd = "clang_delta --transformation=$which --counter=$index $cfile > $tmpfile";
-    my $res = runit ($cmd);
+    my $cmd = "clang_delta --transformation=$which --counter=$index $cfile";
+    my $res = runit ("$cmd > $tmpfile");
     if ($res==0) {
 	system "mv $tmpfile $cfile";
 	return ($OK, \$index);
     } else {
+	if ($res == -1) {
+	} else {
+	    my $tmpfile2 = "clang_delta_crash_${tmpfile}";
+	    system "cp $cfile $tmpfile2";
+	    open TMPF, ">>$tmpfile2";
+	    print TMPF "\n\n$cmd\n";
+	    close TMPF;
+	    print "\n\n=======================================\n\n";
+	    print "OOPS: clang_delta crashed; please consider\n";
+	    print "mailing ${tmpfile2} to creduce-bugs\@flux.utah.edu\n";
+	    print "and we will try to fix the bug\n";
+	    print "\n=======================================\n\n";
+	}
 	system "rm $tmpfile";
 	return ($STOP, \$index);
     }    
