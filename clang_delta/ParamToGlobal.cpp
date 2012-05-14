2b88cf408338a415260533698a5113264e287e61
Yang Chen <chenyang@cs.utah.edu>
2012-05-14 16:35:26 -0600

    use the newly added common visitor

--- clang_delta/ParamToGlobal.cpp
+++ clang_delta/ParamToGlobal.cpp
@@ -10,7 +10,6 @@
 
 #include "ParamToGlobal.h"
 
-#include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Basic/SourceManager.h"
 
 #include "TransformationManager.h"
@@ -43,47 +42,14 @@ private:
 };
 
 class ParamToGlobalRewriteVisitor : public 
-  RecursiveASTVisitor<ParamToGlobalRewriteVisitor> {
+  CommonParameterRewriteVisitor<ParamToGlobalRewriteVisitor, ParamToGlobal> {
 
 public:
-
   explicit ParamToGlobalRewriteVisitor(ParamToGlobal *Instance)
-    : ConsumerInstance(Instance)
+    : CommonParameterRewriteVisitor(Instance)
   { }
 
-  bool VisitCallExpr(CallExpr *E);
-
-  bool VisitFunctionDecl(FunctionDecl *FD);
-
-  bool VisitCXXConstructorDecl(CXXConstructorDecl *CD);
-
   bool VisitDeclRefExpr(DeclRefExpr *ParmRefExpr);
-
-  void rewriteAllExprs(void);
-
-private:
-
-  ParamToGlobal *ConsumerInstance;
-
-  SmallVector<CallExpr *, 10> AllCallExprs;
-
-  SmallVector<const CXXConstructExpr *, 5> AllConstructExprs;
-
-  bool rewriteFuncDecl(FunctionDecl *FP);
-
-  bool rewriteParam(const ParmVarDecl *PV, 
-                    unsigned int NumParams);
-
-  bool rewriteOneCallExpr(CallExpr *CallE);
-
-  bool rewriteOneConstructExpr(const CXXConstructExpr *CE);
-
-  bool makeParamAsGlobalVar(FunctionDecl *FD,
-                            const ParmVarDecl *PV);
-
-  std::string getNewName(FunctionDecl *FP,
-                         const ParmVarDecl *PV);
-
 };
 
 bool ParamToGlobalASTVisitor::VisitFunctionDecl(FunctionDecl *FD)
@@ -94,17 +60,7 @@ bool ParamToGlobalASTVisitor::VisitFunctionDecl(FunctionDecl *FD)
   return true;
 }
 
-bool ParamToGlobalRewriteVisitor::rewriteParam(const ParmVarDecl *PV, 
-                                               unsigned int NumParams)
-{
-  return 
-    ConsumerInstance->RewriteHelper->removeParamFromFuncDecl(PV, 
-                                          NumParams,
-                                          ConsumerInstance->TheParamPos);
-}
-
-std::string ParamToGlobalRewriteVisitor::getNewName(FunctionDecl *FP,
-                                                    const ParmVarDecl *PV)
+std::string ParamToGlobal::getNewName(FunctionDecl *FP, const ParmVarDecl *PV)
 {
   std::string NewName;
   NewName = FP->getNameInfo().getAsString();
@@ -112,155 +68,8 @@ std::string ParamToGlobalRewriteVisitor::getNewName(FunctionDecl *FP,
   NewName += PV->getNameAsString();
 
   // also backup the new name
-  ConsumerInstance->TheNewDeclName = NewName;
+  TheNewDeclName = NewName;
   return NewName;
-}
-
-bool ParamToGlobalRewriteVisitor::makeParamAsGlobalVar(FunctionDecl *FD,
-                                                       const ParmVarDecl *PV)
-{
-  std::string PName = PV->getNameAsString();
-  // Safe to omit an un-named parameter
-  if (PName.empty())
-    return true;
-
-  std::string GlobalVarStr;
-
-  GlobalVarStr = PV->getType().getAsString();
-  GlobalVarStr += " ";
-  GlobalVarStr += getNewName(FD, PV);
-  GlobalVarStr += ";\n";
-
-  return ConsumerInstance->RewriteHelper->insertStringBeforeFunc(FD, 
-                                                       GlobalVarStr);
-}
-
-bool ParamToGlobalRewriteVisitor::rewriteFuncDecl(FunctionDecl *FD) 
-{
-  const ParmVarDecl *PV = 
-    FD->getParamDecl(ConsumerInstance->TheParamPos);  
-
-  TransAssert(PV && "Unmatched ParamPos!");
-  if (!rewriteParam(PV, FD->getNumParams()))
-    return false;
-
-  if (FD->isThisDeclarationADefinition()) {
-    ConsumerInstance->TheParmVarDecl = PV;
-    if (!makeParamAsGlobalVar(FD, PV))
-      return false;
-  }
-  return true;
-}
-
-bool ParamToGlobalRewriteVisitor::VisitCXXConstructorDecl(
-       CXXConstructorDecl *CD)
-{
-  for (CXXConstructorDecl::init_iterator I = CD->init_begin(),
-       E = CD->init_end(); I != E; ++I) {
-    const Expr *InitE = (*I)->getInit();
-    if (!InitE)
-      continue;
-    const CXXConstructExpr *CE = dyn_cast<CXXConstructExpr>(InitE);
-    if (!CE)
-      continue;
-
-    const CXXConstructorDecl *CtorD = CE->getConstructor();
-    if (CtorD->getCanonicalDecl() == ConsumerInstance->TheFuncDecl)
-      AllConstructExprs.push_back(CE);
-  }
-
-  return true;
-}
-
-bool ParamToGlobalRewriteVisitor::VisitFunctionDecl(FunctionDecl *FD)
-{
-  FunctionDecl *CanonicalFD = FD->getCanonicalDecl();
-
-  if (CanonicalFD == ConsumerInstance->TheFuncDecl)
-    return rewriteFuncDecl(FD);
-
-  return true;
-}
-
-bool ParamToGlobalRewriteVisitor::rewriteOneCallExpr(CallExpr *CallE)
-{
-  return 
-    ConsumerInstance->RewriteHelper->removeArgFromCallExpr(CallE, 
-                                        ConsumerInstance->TheParamPos);
-}
-
-bool ParamToGlobalRewriteVisitor::rewriteOneConstructExpr(
-       const CXXConstructExpr *CE)
-{
-  return 
-    ConsumerInstance->RewriteHelper->removeArgFromCXXConstructExpr(CE,
-                                        ConsumerInstance->TheParamPos);
-}
-
-void ParamToGlobalRewriteVisitor::rewriteAllExprs(void)
-{
-  while (!AllCallExprs.empty()) {
-    CallExpr *CallE = AllCallExprs.pop_back_val();
-    rewriteOneCallExpr(CallE);
-  }
-
-  while (!AllConstructExprs.empty()) {
-    const CXXConstructExpr *CE = AllConstructExprs.pop_back_val();
-    rewriteOneConstructExpr(CE);
-  }
-}
-
-bool ParamToGlobalRewriteVisitor::VisitCallExpr(CallExpr *CallE) 
-{
-  const FunctionDecl *CalleeDecl = NULL;
-  const Expr *E = CallE->getCallee();
-  if (const UnresolvedLookupExpr *UE = dyn_cast<UnresolvedLookupExpr>(E)) {
-    DeclarationName DName = UE->getName();
-    TransAssert((DName.getNameKind() == DeclarationName::Identifier) &&
-                "Not an indentifier!");
-    if (const NestedNameSpecifier *NNS = UE->getQualifier()) {
-      if (const DeclContext *Ctx = 
-          ConsumerInstance->getDeclContextFromSpecifier(NNS))
-        CalleeDecl = ConsumerInstance->lookupFunctionDecl(DName, Ctx);
-    }
-    if (!CalleeDecl)
-      CalleeDecl = ConsumerInstance->lookupFunctionDecl(DName, 
-                     ConsumerInstance->TheFuncDecl->getLookupParent());
-    TransAssert(CalleeDecl && "NULL CalleeDecl!");
-  }
-  else {
-    CalleeDecl = CallE->getDirectCallee();
-    if (!CalleeDecl) {
-      return true;
-    }
-  }
-
-  if (FunctionTemplateDecl *TheTmplFuncD = 
-        ConsumerInstance->TheFuncDecl->getDescribedFunctionTemplate()) {
-    FunctionTemplateDecl *TmplFuncD;
-    if (CalleeDecl->isTemplateInstantiation())
-      TmplFuncD = CalleeDecl->getPrimaryTemplate();
-    else 
-      TmplFuncD = CalleeDecl->getDescribedFunctionTemplate();
-    if (!TmplFuncD || 
-        (TmplFuncD->getCanonicalDecl() != TheTmplFuncD->getCanonicalDecl()))
-      return true;
-  }
-  else if (CalleeDecl->getCanonicalDecl() != ConsumerInstance->TheFuncDecl) {
-    return true;
-  }
-
-  // We now have a correct CallExpr
-  // Here we only collect these valid CallExprs, and 
-  // will rewrite them later in a reverse order. 
-  // The reason is that if we have code like below:
-  //    foo(foo(1));
-  // we want to rewrite the nested foo(1) first.
-  // If we rewrite the outside foo first, we will
-  // end up with bad transformation when we try to 
-  // rewrite foo(1), which has been removed. 
-  AllCallExprs.push_back(CallE);
-  return true;
 }
 
 bool ParamToGlobalRewriteVisitor::VisitDeclRefExpr(DeclRefExpr *ParmRefExpr)
@@ -275,8 +84,8 @@ bool ParamToGlobalRewriteVisitor::VisitDeclRefExpr(DeclRefExpr *ParmRefExpr)
 
   SourceRange ExprRange = ParmRefExpr->getSourceRange();
   return 
-    !(ConsumerInstance->TheRewriter.ReplaceText(ExprRange,
-        ConsumerInstance->TheNewDeclName));
+    !(ConsumerInstance->TheRewriter.ReplaceText(ExprRange, 
+                                      ConsumerInstance->TheNewDeclName));
 }
 
 void ParamToGlobal::Initialize(ASTContext &context) 
@@ -298,9 +107,9 @@ void ParamToGlobal::HandleTranslationUnit(ASTContext &Ctx)
   }
 
   TransAssert(RewriteVisitor && "NULL RewriteVisitor!");
-  Ctx.getDiagnostics().setSuppressAllDiagnostics(false);
   TransAssert(TheFuncDecl && "NULL TheFuncDecl!");
   TransAssert((TheParamPos >= 0) && "Invalid parameter position!");
+  Ctx.getDiagnostics().setSuppressAllDiagnostics(false);
 
   RewriteVisitor->TraverseDecl(Ctx.getTranslationUnitDecl());
   RewriteVisitor->rewriteAllExprs();
@@ -308,6 +117,23 @@ void ParamToGlobal::HandleTranslationUnit(ASTContext &Ctx)
   if (Ctx.getDiagnostics().hasErrorOccurred() ||
       Ctx.getDiagnostics().hasFatalErrorOccurred())
     TransError = TransInternalError;
+}
+
+bool ParamToGlobal::transformParamVar(FunctionDecl *FD, const ParmVarDecl *PV)
+{
+  std::string PName = PV->getNameAsString();
+  // Safe to omit an un-named parameter
+  if (PName.empty())
+    return true;
+
+  std::string GlobalVarStr;
+
+  GlobalVarStr = PV->getType().getAsString();
+  GlobalVarStr += " ";
+  GlobalVarStr += getNewName(FD, PV);
+  GlobalVarStr += ";\n";
+
+  return RewriteHelper->insertStringBeforeFunc(FD, GlobalVarStr);
 }
 
 bool ParamToGlobal::isValidFuncDecl(FunctionDecl *FD) 
