d155a6af684a3589837c288e2c8adbd55545ddb0
John Regehr <regehr@cs.utah.edu>
2012-06-28 15:26:46 -0600

    actually make it verbose...

--- creduce/creduce_utils.pm
+++ creduce/creduce_utils.pm
@@ -17,7 +17,7 @@ use Exporter::Lite;
 @EXPORT      = qw(read_file write_file $OK $STOP $VERBOSE
                   $replace_cont replace_aux runit $matched);
 
-$VERBOSE = 0;
+$VERBOSE = 1;
 
 $OK = 999999;
 $STOP = 111333;
