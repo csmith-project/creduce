32b86657a6a391e57735433fb4c52c968a122382
Joerg Sonnenberger <joerg@bec.de>
2012-11-26 13:34:44 +0100

    Redundant ;

--- clang_delta/ReplaceSimpleTypedef.h
+++ clang_delta/ReplaceSimpleTypedef.h
@@ -18,7 +18,7 @@ namespace clang {
   class DeclGroupRef;
   class ASTContext;
   class TypedefDecl;
-};
+}
 
 class ReplaceSimpleTypedefCollectionVisitor;
 class ReplaceSimpleTypedefRewriteVisitor;
