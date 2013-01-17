60c430f8e831ed6aa5945b14e61cfa6e3c5e8c86
Yang Chen <chenyang@cs.utah.edu>
2013-01-16 18:48:37 -0700

    use IK rather than a IK_CXX for InitializeSourceManager

--- clang_delta/TransformationManager.cpp
+++ clang_delta/TransformationManager.cpp
@@ -127,7 +127,7 @@ bool TransformationManager::initializeCompilerInstance(std::string &ErrorMsg)
   PP.getBuiltinInfo().InitializeBuiltins(PP.getIdentifierTable(),
                                          PP.getLangOpts());
 
-  if (!ClangInstance->InitializeSourceManager(FrontendInputFile(SrcFileName, IK_CXX))) {
+  if (!ClangInstance->InitializeSourceManager(FrontendInputFile(SrcFileName, IK))) {
     ErrorMsg = "Cannot open source file!";
     return false;
   }
