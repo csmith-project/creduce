787da6df25a7628e45507f6799b91990035649de
John Regehr <regehr@cs.utah.edu>
2013-06-02 17:01:31 -0600

    oops forgot to add this

---
+++ clex/stuff.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+/*
+ * Stuff defined for us by the lex/flex-generated code.
+ */
+extern FILE *yyin;
+extern int yylex(void);
+extern char *yytext;
+
+/*
+ * Stuff that we define within the lex/flex-generated code.
+ */
+extern int count;
+
+enum tok_kind {
+  TOK_KEYWORD = 999,
+  TOK_OP,
+  TOK_IDENT,
+  TOK_OTHER,
+  TOK_NUMBER
+};
+
+void doit (enum tok_kind);
