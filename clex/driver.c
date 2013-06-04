de1276ad6471f71745ee3abb43ca6c5b5e977882
John Regehr <regehr@cs.utah.edu>
2013-06-03 21:26:49 -0600

    token numbering seems to work

--- clex/driver.c
+++ clex/driver.c
@@ -8,14 +8,16 @@
 struct tok_t {
   char *str;
   enum tok_kind kind;
+  int id;
 };
 
+int next_id;
 struct tok_t *tok_list;
 int toks;
 int max_toks;
 const int initial_length = 1;
 
-void add_tok (char *str, enum tok_kind kind)
+int add_tok (char *str, enum tok_kind kind)
 {
   assert (str);
   if (toks >= max_toks) {
@@ -23,14 +25,35 @@ void add_tok (char *str, enum tok_kind kind)
     tok_list = (struct tok_t *) realloc (tok_list, max_toks * sizeof (struct tok_t));
     assert (tok_list);
   }
-  tok_list[toks].str = str;
+  tok_list[toks].str = strdup (str);
+  assert (tok_list[toks].str);
   tok_list[toks].kind = kind;
+  tok_list[toks].id = -1;
   toks++;
+  return toks-1;
+}
+
+void classify_tok (int tok)
+{
+  if (tok_list[tok].kind != TOK_IDENT) return;
+  int i;
+  for (i=0; i<toks; i++) {
+    if (tok_list[i].kind != TOK_IDENT) continue;
+    if (i==tok) continue;
+    assert (tok_list[i].id != -1);
+    if (strcmp (tok_list[i].str, tok_list[tok].str) == 0) {
+      tok_list[tok].id = tok_list[i].id;
+      return;
+    }
+  }
+  tok_list[tok].id = next_id;
+  next_id++;
 }
 
 void doit (enum tok_kind kind)
 {
-  add_tok (yytext, kind);
+  int tok = add_tok (yytext, kind);
+  classify_tok (tok);
   count++;
 }
 
@@ -48,7 +71,11 @@ int main(int argc, char *argv[]) {
   yylex();
   int i;
   for (i=0; i<toks; i++) {
-    printf ("%s ", tok_list[i].str);
+    printf ("%s", tok_list[i].str);
+    if (tok_list[i].id != -1) {
+      printf (" -=- %d -=-", tok_list[i].id);
+    }
+    printf ("\n");
   }
   printf ("// %d tokens\n", count);
   return 0;
