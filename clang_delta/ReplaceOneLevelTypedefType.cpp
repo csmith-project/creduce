24e3158c0d306e3a7f5337b4c1c1745c0f9aa2ce
Yang Chen <chenyang@cs.utah.edu>
2013-06-30 03:54:56 -0600

    a new transformation --replace-one-level-typedef-type
    literally replace a typedef type with the corresponding
    underlying type string, if the typedef type is only referenced once

---
+++ clang_delta/ReplaceOneLevelTypedefType.cpp
@@ -0,0 +1,147 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (c) 2012 The University of Utah
+// All rights reserved.
+//
+// This file is distributed under the University of Illinois Open Source
+// License.  See the file COPYING for details.
+//
+//===----------------------------------------------------------------------===//
+
+#if HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include "ReplaceOneLevelTypedefType.h"
+
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/Basic/SourceManager.h"
+
+#include "TransformationManager.h"
+
+using namespace clang;
+using namespace llvm;
+
+static const char *DescriptionMsg =
+"This pass literally replaces a typedef type with the underlying type \
+if typedef type is only been used once. It doesn't recursively resolve \
+the underlying type.";
+
+static RegisterTransformation<ReplaceOneLevelTypedefType>
+         Trans("replace-one-level-typedef-type", DescriptionMsg);
+
+class ReplaceOneLevelTypedefTypeCollectionVisitor : public
+  RecursiveASTVisitor<ReplaceOneLevelTypedefTypeCollectionVisitor> {
+
+public:
+  explicit ReplaceOneLevelTypedefTypeCollectionVisitor(
+             ReplaceOneLevelTypedefType *Instance)
+    : ConsumerInstance(Instance)
+  { }
+
+  bool VisitTypedefTypeLoc(TypedefTypeLoc TLoc);
+
+private:
+  ReplaceOneLevelTypedefType *ConsumerInstance;
+};
+
+bool ReplaceOneLevelTypedefTypeCollectionVisitor::VisitTypedefTypeLoc(
+       TypedefTypeLoc TLoc)
+{
+  ConsumerInstance->handleOneTypedefTypeLoc(TLoc);
+  return true;
+}
+
+void ReplaceOneLevelTypedefType::Initialize(ASTContext &context) 
+{
+  Transformation::Initialize(context);
+  CollectionVisitor = new ReplaceOneLevelTypedefTypeCollectionVisitor(this);
+}
+
+void ReplaceOneLevelTypedefType::HandleTranslationUnit(ASTContext &Ctx)
+{
+  CollectionVisitor->TraverseDecl(Ctx.getTranslationUnitDecl());
+  analyzeTypeLocs();
+
+  if (QueryInstanceOnly)
+    return;
+
+  if (TransformationCounter > ValidInstanceNum) {
+    TransError = TransMaxInstanceError;
+    return;
+  }
+
+  Ctx.getDiagnostics().setSuppressAllDiagnostics(false);
+  TransAssert(TheTypedefDecl && "NULL TheTypedefDecl!");
+  rewriteTypedefType();
+  removeTypedefs();
+
+  if (Ctx.getDiagnostics().hasErrorOccurred() ||
+      Ctx.getDiagnostics().hasFatalErrorOccurred())
+    TransError = TransInternalError;
+}
+
+void ReplaceOneLevelTypedefType::analyzeTypeLocs()
+{
+  for (TypedefDeclToRefMap::iterator I = AllTypeDecls.begin(),
+       E = AllTypeDecls.end(); I != E; ++I) {
+    TypedefTypeLocVector *LocVec = (*I).second;
+    if (LocVec->size() > 1)
+      continue;
+    ValidInstanceNum++;
+    if (ValidInstanceNum == TransformationCounter) {
+      TheTypedefDecl = (*I).first;
+      TheTypeLoc = LocVec->back();
+    }
+  }
+}
+
+void ReplaceOneLevelTypedefType::rewriteTypedefType()
+{
+  std::string NewTyStr;
+  TheTypedefDecl->getUnderlyingType().getAsStringInternal(NewTyStr, 
+                                        Context->getPrintingPolicy());
+  SourceRange Range = TheTypeLoc.getSourceRange();
+  TheRewriter.ReplaceText(Range, NewTyStr);
+}
+
+void ReplaceOneLevelTypedefType::removeTypedefs()
+{
+  for (TypedefDecl::redecl_iterator I = TheTypedefDecl->redecls_begin(),
+       E = TheTypedefDecl->redecls_end(); I != E; ++I) {
+    SourceRange Range = (*I)->getSourceRange();
+    if (Range.isValid()) {
+      RewriteHelper->removeTextUntil(Range, ';');
+      Rewritten = true;
+    }
+  }
+}
+
+void ReplaceOneLevelTypedefType::handleOneTypedefTypeLoc(TypedefTypeLoc TLoc)
+{
+  const TypedefType *TdefTy = TLoc.getTypePtr();
+  const TypedefDecl *TdefD = dyn_cast<TypedefDecl>(TdefTy->getDecl());
+  if (!TdefD || TdefD->getLocStart().isInvalid())
+    return;
+  const TypedefDecl *CanonicalD = 
+    dyn_cast<TypedefDecl>(TdefD->getCanonicalDecl());
+
+  TypedefTypeLocVector *LocVec = AllTypeDecls[CanonicalD];
+  if (!LocVec) {
+    LocVec = new TypedefTypeLocVector();
+    TransAssert(LocVec && "NULL LocVec!");
+    AllTypeDecls[CanonicalD] = LocVec;
+  }
+  LocVec->push_back(TLoc);
+}
+
+ReplaceOneLevelTypedefType::~ReplaceOneLevelTypedefType(void)
+{
+  for (TypedefDeclToRefMap::iterator I = AllTypeDecls.begin(),
+       E = AllTypeDecls.end(); I != E; ++I) {
+    delete (*I).second;
+  }
+  delete CollectionVisitor;
+}
+
---
+++ clang_delta/ReplaceOneLevelTypedefType.h
@@ -0,0 +1,76 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (c) 2012 The University of Utah
+// All rights reserved.
+//
+// This file is distributed under the University of Illinois Open Source
+// License.  See the file COPYING for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef REPLACE_ONE_LEVEL_TYPEDEF_TYPE_H
+#define REPLACE_ONE_LEVEL_TYPEDEF_TYPE_H
+
+#include "Transformation.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/SmallVector.h"
+#include "clang/AST/TypeLoc.h"
+
+namespace clang {
+  class DeclGroupRef;
+  class ASTContext;
+  class TypedefDecl;
+}
+
+class ReplaceOneLevelTypedefTypeCollectionVisitor;
+
+class ReplaceOneLevelTypedefType : public Transformation {
+friend class ReplaceOneLevelTypedefTypeCollectionVisitor;
+
+public:
+  ReplaceOneLevelTypedefType(const char *TransName, const char *Desc)
+    : Transformation(TransName, Desc),
+      CollectionVisitor(NULL)
+  {}
+
+  ~ReplaceOneLevelTypedefType(void);
+
+private:
+
+  typedef llvm::SmallVector<clang::TypedefTypeLoc, 10>
+    TypedefTypeLocVector;
+
+  typedef llvm::DenseMap<const clang::TypedefDecl *, 
+                         TypedefTypeLocVector *>
+    TypedefDeclToRefMap;
+
+  virtual void Initialize(clang::ASTContext &context);
+
+  virtual void HandleTranslationUnit(clang::ASTContext &Ctx);
+
+  void handleOneTypedefTypeLoc(clang::TypedefTypeLoc TLoc);
+
+  void analyzeTypeLocs();
+
+  void rewriteTypedefType();
+
+  void removeTypedefs();
+
+  TypedefDeclToRefMap AllTypeDecls;
+
+  clang::TypedefTypeLoc TheTypeLoc;
+
+  ReplaceOneLevelTypedefTypeCollectionVisitor *CollectionVisitor;
+
+  const clang::TypedefDecl *TheTypedefDecl;
+
+  // Unimplemented
+  ReplaceOneLevelTypedefType(void);
+
+  ReplaceOneLevelTypedefType(const ReplaceOneLevelTypedefType &);
+
+  void operator=(const ReplaceOneLevelTypedefType &);
+};
+
+#endif
+
