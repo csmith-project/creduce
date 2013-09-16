7d15576559e10b1b1db539e389500c9daeef6d84
Yang Chen <chenyang@cs.utah.edu>
2013-09-16 10:23:56 -0600

    added support to bin-search unused functions
    * Previously, I misinterpreted isReferenced flags
    for templates. Fixed most of those. There are
    still some cases where I couldn't figure out
    how to resolve. For example,
    
    template<typename T> struct S;
    template<typename T> void foo(S<T>);
    template<> void foo(S<char>);
    template void foo(S<char>);
    
    Seems there is no easy way to retrive the source
    location for the template instantiation at
    line 4. Internally, this instantiation
    is represented as a canonical declaration
    of the explicit specialization at line 3,
    and thus its source location gets lost.
    Note that in this case we couldn't get the instantiation
    point as we did for explicit instantiations.
    
    * added support to bin-search unused functions.

--- clang_delta/RemoveUnusedFunction.h
+++ clang_delta/RemoveUnusedFunction.h
@@ -12,42 +12,176 @@
 #define REMOVE_UNUSED_FUNCTION_H
 
 #include <string>
+#include <map>
+#include <set>
+#include "llvm/ADT/SmallPtrSet.h"
+#include "llvm/ADT/DenseMap.h"
 #include "Transformation.h"
+#include "clang/Basic/SourceLocation.h"
 
 namespace clang {
   class DeclGroupRef;
   class ASTContext;
   class FunctionDecl;
+  class UsingDecl;
+  class DeclarationName;
+  class DeclContext;
+  class CXXDependentScopeMemberExpr;
+  class FunctionTemplateDecl;
+  class UnresolvedLookupExpr;
+  class NestedNameSpecifier;
+  class MemberExpr;
+  class CXXOperatorCallExpr;
 }
 
 class RUFAnalysisVisitor;
+class ExtraReferenceVisitorWrapper;
 
 class RemoveUnusedFunction : public Transformation {
 friend class RUFAnalysisVisitor;
+friend class ExtraReferenceVisitorWrapper;
 
 public:
 
   RemoveUnusedFunction(const char *TransName, const char *Desc)
-    : Transformation(TransName, Desc),
+    : Transformation(TransName, Desc, /*MultipleRewrites*/true),
       AnalysisVisitor(NULL),
+      VisitorWrapper(NULL),
       TheFunctionDecl(NULL)
   { }
 
   ~RemoveUnusedFunction();
 
+  void handleOneUsingDecl(const clang::FunctionDecl *CurrentFD, 
+                          const clang::UsingDecl *D);
+
+  void handleOneCXXDependentScopeMemberExpr(
+         const clang::FunctionDecl *CurrentFD, 
+         const clang::CXXDependentScopeMemberExpr *E);
+
+  void handleOneUnresolvedLookupExpr(
+         const clang::FunctionDecl *CurrentFD,
+         const clang::UnresolvedLookupExpr *E);
+
+  void handleOneMemberExpr(const clang::MemberExpr *ME);
+
+  void handleOneCXXOperatorCallExpr(const clang::CXXOperatorCallExpr *E);
+
+  void handleOneCallExpr(const clang::CallExpr *E);
+
+  void handleOneFunctionDecl(const clang::FunctionDecl *FD);
+
 private:
+
+  typedef llvm::SmallVector<const clang::FunctionDecl *, 1000>
+            FunctionDeclVector;
+
+  typedef llvm::DenseMap<const clang::UsingDecl *, 
+                         const clang::FunctionDecl *>
+            UsingFunctionDeclsMap;
+
+  typedef llvm::SmallPtrSet<const clang::FunctionDecl *, 50>
+            FunctionDeclsSet;
+
+  typedef llvm::SmallPtrSet<const clang::FunctionDecl *, 5>
+            MemberSpecializationSet;
+
+  typedef llvm::DenseMap<const clang::FunctionDecl *, 
+                         MemberSpecializationSet *>
+            MemberToSpecializationMap;
+
+  typedef llvm::SmallPtrSet<const clang::UsingDecl *, 50>
+            UsingDeclsSet;
   
+  typedef std::map<std::string, std::string>
+            InlinedSystemFunctionsMap;
+
+  typedef std::set<std::string> SystemFunctionsSet;
+
   virtual void Initialize(clang::ASTContext &context);
 
   virtual void HandleTranslationUnit(clang::ASTContext &Ctx);
 
+  void initializeInlinedSystemFunctions();
+
   void doRewriting();
 
-  void removeOneFunctionDecl();
+  bool hasReferencedSpecialization(const clang::FunctionDecl *FD);
+
+  clang::SourceLocation getExtensionLocStart(clang::SourceLocation Loc);
+
+  void removeOneFunctionDecl(const clang::FunctionDecl *FD);
+
+  void removeMemberSpecializations(const clang::FunctionDecl *FD);
+
+  clang::SourceLocation getFunctionOuterLocStart(const clang::FunctionDecl *FD);
+
+  bool hasValidOuterLocStart(const clang::FunctionTemplateDecl *FTD, 
+                             const clang::FunctionDecl *FD);
+
+  void removeOneFunctionDeclGroup(const clang::FunctionDecl *FD);
+
+  bool isInReferencedSet(const clang::FunctionDecl *FD);
+
+  void addOneFunctionDecl(const clang::FunctionDecl *CanonicalFD);
+
+  const clang::FunctionDecl *lookupFunctionDeclShallow(
+          const clang::DeclarationName &DName,
+          const clang::DeclContext *Ctx);
+
+  const clang::FunctionDecl *getFunctionDeclFromSpecifier(
+          const clang::DeclarationName &Name, 
+          const clang::NestedNameSpecifier *NNS);
+
+  void addOneReferencedFunction(const clang::FunctionDecl *FD);
+
+  clang::SourceLocation getFunctionLocEnd(clang::SourceLocation LocStart,
+                                          clang::SourceLocation LocEnd,
+                                          const clang::FunctionDecl *FD);
+
+  bool isTokenOperator(clang::SourceLocation Loc);
+
+  void removeFunctionExplicitInstantiations(const clang::FunctionDecl *FD);
+
+  void removeOneExplicitInstantiation(const clang::FunctionDecl *Spec);
+
+  const clang::FunctionTemplateDecl *getTopDescribedTemplate(
+          const clang::FunctionDecl *FD);
+
+  const clang::FunctionDecl *getSourceFunctionDecl(
+          const clang::FunctionDecl *TheFD);
+
+  void setInlinedSystemFunctions(const clang::FunctionDecl *FD);
+
+  bool isInlinedSystemFunction(const clang::FunctionDecl *FD);
+
+  UsingFunctionDeclsMap UsingFDs;
+
+  UsingFunctionDeclsMap UsingParentFDs;
+
+  FunctionDeclsSet ReferencedFDs;
+
+  FunctionDeclsSet VisitedFDs;
+
+  FunctionDeclsSet RemovedFDs;
+
+  UsingDeclsSet VisitedUsingDecls;
+
+  MemberToSpecializationMap MemberToSpecs;
+
+  MemberToSpecializationMap MemberToInstantiations;
+
+  InlinedSystemFunctionsMap InlinedSystemFunctions;
+
+  SystemFunctionsSet ExistingSystemFunctions;
+
+  FunctionDeclVector AllValidFunctionDecls;
 
   RUFAnalysisVisitor *AnalysisVisitor;
 
-  clang::FunctionDecl *TheFunctionDecl;
+  ExtraReferenceVisitorWrapper *VisitorWrapper;
+
+  const clang::FunctionDecl *TheFunctionDecl;
 
   // Unimplemented
   RemoveUnusedFunction();
