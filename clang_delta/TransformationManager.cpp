b2c7b1c960367ba22a6cb9ca6a1931af3f708cc7
Yang Chen <chenyang@cs.utah.edu>
2014-07-18 00:23:02 -0700

    Fixed an API compatibility issue
    Now working with llvm-r213171

--- clang_delta/TransformationManager.cpp
+++ clang_delta/TransformationManager.cpp
@@ -112,7 +112,7 @@ bool TransformationManager::initializeCompilerInstance(std::string &ErrorMsg)
   TargetOpts.Triple = LLVM_DEFAULT_TARGET_TRIPLE;
   TargetInfo *Target = 
     TargetInfo::CreateTargetInfo(ClangInstance->getDiagnostics(),
-                                 &TargetOpts);
+                                 ClangInstance->getInvocation().TargetOpts);
   ClangInstance->setTarget(Target);
   ClangInstance->createFileManager();
   ClangInstance->createSourceManager(ClangInstance->getFileManager());
