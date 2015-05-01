69bb0ce1f0093e6ce25fd0f7c4617872513f1f6d
Eric Eide <eeide@cs.utah.edu>
2015-05-01 16:15:53 -0600

    Update copyright years.

--- standalone/make_standalone.pl
+++ standalone/make_standalone.pl
@@ -1,4 +1,12 @@
 #!/usr/bin/perl -w
+##
+## Copyright (c) 2015 The University of Utah
+## All rights reserved.
+##
+## This file is distributed under the University of Illinois Open Source
+## License.  See the file COPYING for details.
+
+###############################################################################
 
 use strict;
 
@@ -7,3 +15,6 @@ my @progs = ("clang_delta", "clex", "strlex");
 
 my $target = $ARGV[0];
 
+###############################################################################
+
+## End of file.
