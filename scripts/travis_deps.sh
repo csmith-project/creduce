edb9ba5c51a3db3bcb3503b4f4b9c477a5fbad7c
Eric Eide <eeide@cs.utah.edu>
2015-07-22 17:15:07 -0600

    Clean up the Travis-CI configuration files.

--- scripts/travis_deps.sh
+++ scripts/travis_deps.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 ##
+## Copyright (c) 2015 The University of Utah
 ## Copyright (c) 2015 Tamir Duberstein
 ## All rights reserved.
 ##
@@ -9,16 +10,50 @@
 
 ###############################################################################
 
+# This script is run under "sudo".  See file ".travis.yml".
+
 set -eux
 
-apt-get update
-apt-get install -y indent astyle delta libbenchmark-timer-perl \
-  libexporter-lite-perl libfile-which-perl libgetopt-tabular-perl \
-  libregexp-common-perl libsys-cpu-perl flex build-essential libz-dev
+apt-get update -qq
 
-apt-get install -y python-software-properties wget
-add-apt-repository -y ppa:ubuntu-toolchain-r/test
-add-apt-repository -y 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.6 main'
+# Install programs "add-apt-repository" and "wget", needed below.
+apt-get install -y -qq \
+    python-software-properties \
+    wget
+
+# Set up for installing LLVM 3.6.
+# See <https://wiki.ubuntu.com/ToolChain>.
+# See <http://llvm.org/apt/>.
+add-apt-repository -y \
+    ppa:ubuntu-toolchain-r/test
+add-apt-repository -y \
+    'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.6 main'
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | apt-key add -
-apt-get update
-apt-get install -y llvm-3.6 clang-3.6 libclang-3.6-dev clang-format-3.6 libedit-dev
+apt-get update -qq
+
+# Install LLVM 3.6.  See file "INSTALL".
+apt-get install -y -qq \
+    llvm-3.6 \
+    clang-3.6 \
+    libclang-3.6-dev \
+    clang-format-3.6 \
+    libedit-dev
+
+# Install other C-Reduce dependencies.  See file "INSTALL".
+apt-get install -y -qq \
+    indent \
+    astyle \
+    delta \
+    libbenchmark-timer-perl \
+    libexporter-lite-perl \
+    libfile-which-perl \
+    libgetopt-tabular-perl \
+    libregexp-common-perl \
+    libsys-cpu-perl \
+    flex \
+    build-essential \
+    libz-dev
+
+###############################################################################
+
+## End of file.
