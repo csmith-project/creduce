1e2db9d268fd8282203aaba59fb7fba255883c39
Yang Chen <chenyang@cs.utah.edu>
2015-08-07 21:23:05 -0700

    Minor style fix
    use two-space indent instead of four

--- clang_delta/TransformationManager.cpp
+++ clang_delta/TransformationManager.cpp
@@ -98,30 +98,30 @@ bool TransformationManager::initializeCompilerInstance(std::string &ErrorMsg)
     Invocation.setLangDefaults(ClangInstance->getLangOpts(), IK_CXX);
   }
   else if(IK == IK_OpenCL) {
-      //Commandline parameters
-      std::vector<const char*> Args;
-      Args.push_back("-x");
-      Args.push_back("cl");
-      Args.push_back("-Dcl_clang_storage_class_specifiers");
+    //Commandline parameters
+    std::vector<const char*> Args;
+    Args.push_back("-x");
+    Args.push_back("cl");
+    Args.push_back("-Dcl_clang_storage_class_specifiers");
 
-      const char *CLCPath = getenv("CREDUCE_LIBCLC_INCLUDE_PATH");
+    const char *CLCPath = getenv("CREDUCE_LIBCLC_INCLUDE_PATH");
 
-      ClangInstance->createFileManager();
+    ClangInstance->createFileManager();
 
-      if(CLCPath != NULL && ClangInstance->hasFileManager() &&
-         ClangInstance->getFileManager().getDirectory(CLCPath, false) != NULL) {
-          Args.push_back("-I");
-          Args.push_back(CLCPath);
-      }
+    if(CLCPath != NULL && ClangInstance->hasFileManager() &&
+       ClangInstance->getFileManager().getDirectory(CLCPath, false) != NULL) {
+        Args.push_back("-I");
+        Args.push_back(CLCPath);
+    }
 
-      Args.push_back("-include");
-      Args.push_back("clc/clc.h");
-      Args.push_back("-fno-builtin");
+    Args.push_back("-include");
+    Args.push_back("clc/clc.h");
+    Args.push_back("-fno-builtin");
 
-      CompilerInvocation::CreateFromArgs(Invocation,
-                                         &Args[0], &Args[0] + Args.size(),
-                                         ClangInstance->getDiagnostics());
-      Invocation.setLangDefaults(ClangInstance->getLangOpts(), IK_OpenCL);
+    CompilerInvocation::CreateFromArgs(Invocation,
+                                       &Args[0], &Args[0] + Args.size(),
+                                       ClangInstance->getDiagnostics());
+    Invocation.setLangDefaults(ClangInstance->getLangOpts(), IK_OpenCL);
   }
   else {
     ErrorMsg = "Unsupported file type!";
