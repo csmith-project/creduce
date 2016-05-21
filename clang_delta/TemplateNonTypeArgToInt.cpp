f448b5effd20bba7172450eb0d0177a85b6c5980
Yang Chen <chenyang@cs.utah.edu>
2016-05-21 00:42:07 -0700

    Fixed an issue of null-rewriting
    Skip non-type template parameters of "int" type

--- clang_delta/TemplateNonTypeArgToInt.cpp
+++ clang_delta/TemplateNonTypeArgToInt.cpp
@@ -213,8 +213,8 @@ bool TemplateNonTypeArgToInt::isValidParameter(const NamedDecl *ND)
           dyn_cast<NonTypeTemplateParmDecl>(ND);
   if (!NonTypeD)
     return false;
-  // To avoid something such as replace int with int.
-  if (!ND->getIdentifier() && (NonTypeD->getType().getAsString() == "int"))
+  // To avoid something like replacing int with int.
+  if (NonTypeD->getType().getAsString() == "int")
     return false;
   const Type *Ty = NonTypeD->getType().getTypePtr();
   return Ty->isIntegerType();
