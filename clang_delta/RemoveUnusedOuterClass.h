abe05a957ee02875de7d0bd0e9c5b14842353af5
Eric Eide <eeide@cs.utah.edu>
2016-09-12 16:59:47 -0600

    Update copyright years.

--- clang_delta/RemoveUnusedOuterClass.h
+++ clang_delta/RemoveUnusedOuterClass.h
@@ -1,6 +1,6 @@
 //===----------------------------------------------------------------------===//
 //
-// Copyright (c) 2012, 2013 The University of Utah
+// Copyright (c) 2012, 2013, 2016 The University of Utah
 // All rights reserved.
 //
 // This file is distributed under the University of Illinois Open Source
