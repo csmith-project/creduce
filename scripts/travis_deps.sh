7e32dc2ca0a87f56573e4fedc9ea029f46031cec
Eric Eide <eeide@cs.utah.edu>
2017-03-15 09:10:24 -0600

    Require LLVM 4.0.

--- scripts/travis_deps.sh
+++ scripts/travis_deps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 ##
-## Copyright (c) 2015, 2016 The University of Utah
+## Copyright (c) 2015, 2016, 2017 The University of Utah
 ## Copyright (c) 2015 Tamir Duberstein
 ## All rights reserved.
 ##
@@ -28,23 +28,23 @@ apt-get install -y -qq \
     software-properties-common \
     wget
 
-# Set up for installing LLVM 3.9.
+# Set up for installing LLVM 4.0.
 # See <https://wiki.ubuntu.com/ToolChain>.
 # See <http://llvm.org/apt/>.
 add-apt-repository -y \
     ppa:ubuntu-toolchain-r/test
 add-apt-repository -y \
-    'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-3.9 main'
+    'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-4.0 main'
 wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
 apt-get update -qq
 
-# Install LLVM 3.9.  See file "INSTALL".
+# Install LLVM 4.0.  See file "INSTALL".
 apt-get install -y -qq \
-    llvm-3.9 \
-    llvm-3.9-dev \
-    clang-3.9 \
-    libclang-3.9-dev \
-    clang-format-3.9 \
+    llvm-4.0 \
+    llvm-4.0-dev \
+    clang-4.0 \
+    libclang-4.0-dev \
+    clang-format-4.0 \
     libedit-dev
 
 # Install other C-Reduce dependencies.  See file "INSTALL".
